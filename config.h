076f5d49d702069f73b62b64e74e4a4a084efd6d
simpdiletant <64396439+simpdiletant@users.noreply.github.com>
2020-04-27 10:43:12 +0300

    config.h
    Config file

---
+++ config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+#define F_CPU              8000000UL
+#define ONE_WIRE_PORT      PORTB
+#define ONE_WIRE_DDR       DDRB
+#define ONE_WIRE_PIN       PINB
+
+#endif /* CONFIG_H_ */
